a63732de5df892573a30774f23cd7317df3354e2
AspireToCode15 <i_m_sadiq@outlook.com>
2025-08-24 23:28:28 +0530

    solved the port issue

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -o errexit
 
 pip install -r requirements.txt
-python -c "import nltk; nltk.download('punkt', download_dir='nltk_data')"
+python -c "from rembg import remove; from PIL import Image; remove(Image.new('RGB', (1,1)))"
